d9835fbfced61dd37a84067161ab887d71744f6f
GabeSmythe <gabriel.smythe@hotmail.com>
2024-08-15 20:11:13 +1000

    Added calibration offsets for sero_zeroing and move.py (move untested)

--- picar-x/example/1.move.py
+++ picar-x/example/1.move.py
@@ -1,5 +1,14 @@
+import yaml
 from picarx import Picarx
 import time
+
+# Load configuration from config.yaml
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+    
+
+motorIndexes = config['motorIndexes']
+motorOffsets = config['motorOffsets']
 
 
 if __name__ == "__main__":
@@ -12,34 +21,34 @@ if __name__ == "__main__":
         time.sleep(0.5)
         # test direction servo
         for angle in range(0, 35):
-            px.set_dir_servo_angle(angle)
+            px.set_dir_servo_angle(angle + motorOffsets["steer"])
             time.sleep(0.01)
         for angle in range(35, -35, -1):
-            px.set_dir_servo_angle(angle)
+            px.set_dir_servo_angle(angle + motorOffsets["steer"])
             time.sleep(0.01)
         for angle in range(-35, 0):
-            px.set_dir_servo_angle(angle)
+            px.set_dir_servo_angle(angle + motorOffsets["steer"])
             time.sleep(0.01)
         px.stop()
         time.sleep(1)
         # test cam servos
         for angle in range(0, 35):
-            px.set_cam_pan_angle(angle)
+            px.set_cam_pan_angle(angle + motorOffsets["pan"])
             time.sleep(0.01)
         for angle in range(35, -35, -1):
-            px.set_cam_pan_angle(angle)
+            px.set_cam_pan_angle(angle + motorOffsets["pan"])
             time.sleep(0.01)        
         for angle in range(-35, 0):
-            px.set_cam_pan_angle(angle)
+            px.set_cam_pan_angle(angle + motorOffsets["pan"])
             time.sleep(0.01)
         for angle in range(0, 35):
-            px.set_cam_tilt_angle(angle)
+            px.set_cam_tilt_angle(angle + motorOffsets["pan"])
             time.sleep(0.01)
         for angle in range(35, -35,-1):
-            px.set_cam_tilt_angle(angle)
+            px.set_cam_tilt_angle(angle + motorOffsets["pan"])
             time.sleep(0.01)        
         for angle in range(-35, 0):
-            px.set_cam_tilt_angle(angle)
+            px.set_cam_tilt_angle(angle + motorOffsets["pan"])
             time.sleep(0.01)
     finally:
         px.stop()
--- picar-x/example/servo_zeroing.py
+++ picar-x/example/servo_zeroing.py
@@ -1,18 +1,32 @@
-
+import yaml
 from robot_hat import Servo
 from robot_hat.utils import reset_mcu
 from time import sleep
+
+
+# Load configuration from config.yaml
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+    
+
+motorIndexes = config['motorIndexes']
+motorOffsets = config['motorOffsets']
+
 
 reset_mcu()
 sleep(0.2)
 
 if __name__ == '__main__':
-    print(f"Set servo to zero")
     for i in range(12):
-        # print(f"Servo {i} set to zero")
-        Servo(i).angle(10)
-        sleep(0.1)
-        Servo(i).angle(0)
-        sleep(0.1)
+        motorName = motorIndexes.get(i)
+        if motorName is not None:
+            zeroAngle = 0 + motorOffsets.get(motorName, 0)
+            print(f"Servo {motorName} set to {zeroAngle}")
+            Servo(i).angle(10)
+            sleep(0.1)
+            Servo(i).angle(zeroAngle)
+            sleep(5)
+    else:
+        print(f"Servo index {i} is unused")
     while True:
         sleep(1)
